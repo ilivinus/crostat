e525a3699552f9778d9f424e55472457b39e7e5f
Umeaduma Livinus <umeadumalivinus@gmail.com>
2019-08-20 22:03:08 +0100

    addded redux persist to help persist the setting reducer and moved the redux-logger to package.json dev section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 //import {Platform, StyleSheet, Text, View} from 'react-native';
 import AppContainer from './containers/AppContainer'
-import rootStore from './redux/stores';
+import { store, persistor } from './redux/stores';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/commons/Loading';
 
 export default class RootApp extends Component {
-  render(){
-    return(
-    <Provider store={rootStore}>
-      <AppContainer />
-    </Provider>);
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate loading={<Loading/>}  persistor={persistor} >
+          <AppContainer />
+        </PersistGate>
+      </Provider>);
   }
 }
--- src/redux/stores/index.js
+++ src/redux/stores/index.js
@@ -2,9 +2,17 @@ import { createStore, applyMiddleware , combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/es/storage';
 
-
+const persistConfig  = {
+    key :'crostat:root',
+    storage,
+    whitelist : ['setting']
+}
 const middleware = [logger,thunk];
-const store = createStore(combineReducers(rootReducer),applyMiddleware(...middleware));
+const persistedRootReducer = persistReducer(persistConfig,combineReducers(rootReducer));
+const store = createStore(persistedRootReducer,applyMiddleware(...middleware));
+const persistor = persistStore(store);
 
-export default store;
+export  { store, persistor };
